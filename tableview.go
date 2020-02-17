c53f37e4434ec0661f389db766899647b563eed8
Juan Cespedes <cespedes@thehackers.org>
2020-02-17 16:27:51 +0100

    Prepared for multiple tables (untested).

--- tableview.go
+++ tableview.go
@@ -7,6 +7,7 @@ package tableview
 import (
 	"fmt"
 	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/gdamore/tcell"
@@ -21,8 +22,8 @@ type tableViewCommand struct {
 
 // TableView holds a description of one table to be displayed
 type TableView struct {
-	name       string // name of this table, to be able to refer to it
-	app        *tview.Application
+	app        *Application
+	ID         int // index of this table in parent Application's "tables"
 	flex       *tview.Flex
 	table      *tview.Table
 	columns    []string
@@ -35,6 +36,7 @@ type TableView struct {
 	orderCols  []int  // columns to show, and in which order
 	selectCols bool   // selecting columns instead of rows
 	commands   []tableViewCommand
+	legend     *tview.TextView
 	lastLine   tview.Primitive
 }
 
@@ -44,36 +46,203 @@ type Application struct {
 	tables []*TableView
 }
 
-func (a *Application) Run() {
+var DefaultApplication = NewApplication()
+
+func NewApplication() *Application {
+	a := new(Application)
 	a.app = tview.NewApplication()
 	a.pages = tview.NewPages()
-	for i := range a.tables {
-		a.pages.AddPage(a.tables[i].name, a.tables[i].flex, false, i == 0)
+	a.app.SetRoot(a.pages, true)
+	return a
+}
+
+func (a *Application) Run() {
+	if err := a.app.Run(); err != nil {
+		panic(err)
 	}
 }
 
-func (a *Application) NewTable(name string) *TableView {
-	t := NewTableView()
-	t.name = name
-	a.tables = append(a.tables, t)
-	return t
-}
-
-// NewTableView returns an empty TableView
-func NewTableView() *TableView {
+func (a *Application) NewTable() *TableView {
 	t := new(TableView)
+	t.app = a
+	a.tables = append(a.tables, t)
+	t.ID = len(a.tables) - 1
+
 	t.table = tview.NewTable()
 	t.table.SetEvaluateAllRows(true)
 	t.table.SetSeparator(tview.Borders.Vertical)
 	t.table.SetFixed(1, 0)
 	t.table.SetSelectable(true, false)
 	t.flex = tview.NewFlex()
+	t.legend = tview.NewTextView()
+	t.legend.SetBackgroundColor(tcell.ColorBlue)
+	t.legend.SetDynamicColors(true)
+
+	var lastSearch string
+
+	defaultMenu := " [yellow]q:quit   /:search   n:next   f:filter   c:columns"
+	for _, c := range t.commands {
+		defaultMenu = fmt.Sprintf("%s   %c:%s", defaultMenu, c.ch, c.text)
+	}
+	columnsMenu := " [yellow]q:quit   c:back   <:left   >:right   s:sort"
+	t.legend.SetText(defaultMenu)
+	t.table.SetDoneFunc(func(key tcell.Key) {
+		t.app.app.Stop()
+	})
+
+	t.table.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyRune:
+			if t.selectCols {
+				switch event.Rune() {
+				case 'q':
+					t.app.app.Stop()
+					return nil
+				case 'c':
+					t.selectCols = false
+					t.legend.SetText(defaultMenu)
+					t.table.SetSelectable(true, false)
+				case '<':
+					row, col := t.table.GetSelection()
+					if col == 0 {
+						break
+					}
+					t.orderCols[col-1], t.orderCols[col] = t.orderCols[col], t.orderCols[col-1]
+					t.table.Select(row, col-1)
+					t.fillTable()
+				case '>':
+					row, col := t.table.GetSelection()
+					if col == len(t.orderCols)-1 {
+						break
+					}
+					t.orderCols[col], t.orderCols[col+1] = t.orderCols[col+1], t.orderCols[col]
+					t.table.Select(row, col+1)
+					t.fillTable()
+				case 's':
+					_, col := t.table.GetSelection()
+					sort.Slice(t.orderRows, func(a, b int) bool {
+						return t.data[t.orderRows[a]][t.orderCols[col]] < t.data[t.orderRows[b]][t.orderCols[col]]
+					})
+					t.fillTable()
+				}
+				return event
+			}
+			switch event.Rune() {
+			case 'q':
+				t.app.app.Stop()
+				return nil
+			case 'c':
+				t.selectCols = true
+				t.legend.SetText(columnsMenu)
+				t.table.SetSelectable(false, true)
+			case '=':
+				size := len(t.orderRows)
+				_, _, wid, hei := t.table.GetInnerRect()
+				sel, _ := t.table.GetSelection()
+				off, _ := t.table.GetOffset()
+				t.flex.RemoveItem(t.lastLine)
+				t.lastLine = tview.NewTextView().SetText(fmt.Sprintf("size=%d wid=%d hei=%d sel=%d off=%d", size, wid, hei, sel, off))
+				t.flex.AddItem(t.lastLine, 1, 0, false)
+			case '<':
+				row, col := t.table.GetSelection()
+				offh, offv := t.table.GetOffset()
+				_, _, _, hei := t.table.GetInnerRect()
+				if offh > 0 {
+					if row-offh+1 == hei {
+						t.table.Select(row-1, col)
+					}
+					t.table.SetOffset(offh-1, offv)
+				}
+			case '>':
+				row, col := t.table.GetSelection()
+				offh, offv := t.table.GetOffset()
+				_, _, _, hei := t.table.GetInnerRect()
+				if (row == offh+1) && (offh+hei <= len(t.data)) {
+					t.table.Select(row+1, col)
+				}
+				t.table.SetOffset(offh+1, offv)
+			case '/':
+				row, _ := t.table.GetSelection()
+				row--
+				search := tview.NewInputField()
+				search.SetLabel("Search: ")
+				search.SetFieldBackgroundColor((tcell.ColorBlack))
+				search.SetChangedFunc(func(text string) {
+					if t.search(row, text) {
+						search.SetFieldTextColor(tcell.ColorWhite)
+					} else {
+						search.SetFieldTextColor((tcell.ColorRed))
+					}
+				})
+				search.SetDoneFunc(func(key tcell.Key) {
+					lastSearch = search.GetText()
+					t.updateLastLine()
+					t.app.app.SetFocus(t.table)
+				})
+				t.flex.RemoveItem(t.lastLine)
+				t.lastLine = search
+				t.flex.AddItem(t.lastLine, 1, 0, false)
+				t.app.app.SetFocus(search)
+			case 'n':
+				row, _ := t.table.GetSelection()
+				t.search(row, lastSearch)
+			case 'f':
+				row, _ := t.table.GetSelection()
+				row--
+				line := tview.NewInputField()
+				line.SetLabel("Filter: ")
+				t.filter = ""
+				t.filterData()
+				line.SetFieldBackgroundColor((tcell.ColorBlack))
+				line.SetChangedFunc(func(text string) {
+					t.filter = text
+					t.filterData()
+				})
+				line.SetDoneFunc(func(key tcell.Key) {
+					t.filter = line.GetText()
+					t.filterData()
+					t.updateLastLine()
+					t.app.app.SetFocus(t.table)
+				})
+				t.flex.RemoveItem(t.lastLine)
+				t.lastLine = line
+				t.flex.AddItem(t.lastLine, 1, 0, false)
+				t.app.app.SetFocus(line)
+			}
+			for _, c := range t.commands {
+				if event.Rune() == c.ch {
+					row, _ := t.table.GetSelection()
+					t.app.app.Suspend(func() {
+						c.action(t.orderRows[row-1])
+						t.fillTable()
+					})
+				}
+			}
+		}
+		return event
+	})
+
+	t.flex.SetBackgroundColor(tcell.ColorRed)
+	t.flex.SetDirection(tview.FlexRow)
+	t.flex.AddItem(t.table, 0, 1, true)
+	t.flex.AddItem(t.legend, 1, 0, false)
+	t.lastLine = tview.NewBox()
+	t.flex.AddItem(t.lastLine, 1, 0, false)
+
+	a.pages.AddPage(strconv.Itoa(t.ID), a.tables[t.ID].flex, true, false)
+	a.SetActiveTable(t)
 	return t
 }
 
-func NewApplication() *Application {
-	a := new(Application)
-	return a
+func (a *Application) SetActiveTable(t *TableView) {
+	a.pages.SwitchToPage(strconv.Itoa(t.ID))
+	a.app.SetFocus(t.table)
+}
+
+// NewTableView returns an empty TableView
+func NewTableView() *TableView {
+	a := DefaultApplication
+	return a.NewTable()
 }
 
 // FillTable populates a TableView with the given data
@@ -216,7 +385,7 @@ func (t *TableView) NewCommand(ch rune, text string, action func(row int)) {
 // presses ENTER.  The selecred row is passed to the function as an argument.
 func (t *TableView) SetSelectedFunc(action func(row int)) {
 	t.table.SetSelectedFunc(func(row int, col int) {
-		t.app.Suspend(func() {
+		t.app.app.Suspend(func() {
 			action(row)
 		})
 	})
@@ -259,161 +428,7 @@ func (t *TableView) search(startRow int, text string) bool {
 // Run draws the table and starts a loop, waiting for keystrokes
 // and redrawing the screen.  It exits when ^C or "q" is pressed.
 func (t *TableView) Run() {
-	t.app = tview.NewApplication()
-	text := tview.NewTextView()
-	var lastSearch string
-
-	text.SetBackgroundColor(tcell.ColorBlue)
-	text.SetDynamicColors(true)
-	defaultMenu := " [yellow]q:quit   /:search   n:next   f:filter   c:columns"
-	for _, c := range t.commands {
-		defaultMenu = fmt.Sprintf("%s   %c:%s", defaultMenu, c.ch, c.text)
-	}
-	columnsMenu := " [yellow]q:quit   c:back   <:left   >:right   s:sort"
-	text.SetText(defaultMenu)
+	t.app.SetActiveTable(t)
 	t.fillTable()
-	t.table.SetDoneFunc(func(key tcell.Key) {
-		t.app.Stop()
-	})
-	t.table.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Key() {
-		case tcell.KeyRune:
-			if t.selectCols {
-				switch event.Rune() {
-				case 'q':
-					t.app.Stop()
-					return nil
-				case 'c':
-					t.selectCols = false
-					text.SetText(defaultMenu)
-					t.table.SetSelectable(true, false)
-				case '<':
-					row, col := t.table.GetSelection()
-					if col == 0 {
-						break
-					}
-					t.orderCols[col-1], t.orderCols[col] = t.orderCols[col], t.orderCols[col-1]
-					t.table.Select(row, col-1)
-					t.fillTable()
-				case '>':
-					row, col := t.table.GetSelection()
-					if col == len(t.orderCols)-1 {
-						break
-					}
-					t.orderCols[col], t.orderCols[col+1] = t.orderCols[col+1], t.orderCols[col]
-					t.table.Select(row, col+1)
-					t.fillTable()
-				case 's':
-					_, col := t.table.GetSelection()
-					sort.Slice(t.orderRows, func(a, b int) bool {
-						return t.data[t.orderRows[a]][t.orderCols[col]] < t.data[t.orderRows[b]][t.orderCols[col]]
-					})
-					t.fillTable()
-				}
-				return event
-			}
-			switch event.Rune() {
-			case 'q':
-				t.app.Stop()
-				return nil
-			case 'c':
-				t.selectCols = true
-				text.SetText(columnsMenu)
-				t.table.SetSelectable(false, true)
-			case '=':
-				size := len(t.orderRows)
-				_, _, wid, hei := t.table.GetInnerRect()
-				sel, _ := t.table.GetSelection()
-				off, _ := t.table.GetOffset()
-				t.flex.RemoveItem(t.lastLine)
-				t.lastLine = tview.NewTextView().SetText(fmt.Sprintf("size=%d wid=%d hei=%d sel=%d off=%d", size, wid, hei, sel, off))
-				t.flex.AddItem(t.lastLine, 1, 0, false)
-			case '<':
-				row, col := t.table.GetSelection()
-				offh, offv := t.table.GetOffset()
-				_, _, _, hei := t.table.GetInnerRect()
-				if offh > 0 {
-					if row-offh+1 == hei {
-						t.table.Select(row-1, col)
-					}
-					t.table.SetOffset(offh-1, offv)
-				}
-			case '>':
-				row, col := t.table.GetSelection()
-				offh, offv := t.table.GetOffset()
-				_, _, _, hei := t.table.GetInnerRect()
-				if (row == offh+1) && (offh+hei <= len(t.data)) {
-					t.table.Select(row+1, col)
-				}
-				t.table.SetOffset(offh+1, offv)
-			case '/':
-				row, _ := t.table.GetSelection()
-				row--
-				search := tview.NewInputField()
-				search.SetLabel("Search: ")
-				search.SetFieldBackgroundColor((tcell.ColorBlack))
-				search.SetChangedFunc(func(text string) {
-					if t.search(row, text) {
-						search.SetFieldTextColor(tcell.ColorWhite)
-					} else {
-						search.SetFieldTextColor((tcell.ColorRed))
-					}
-				})
-				search.SetDoneFunc(func(key tcell.Key) {
-					lastSearch = search.GetText()
-					t.updateLastLine()
-					t.app.SetFocus(t.table)
-				})
-				t.flex.RemoveItem(t.lastLine)
-				t.lastLine = search
-				t.flex.AddItem(t.lastLine, 1, 0, false)
-				t.app.SetFocus(search)
-			case 'n':
-				row, _ := t.table.GetSelection()
-				t.search(row, lastSearch)
-			case 'f':
-				row, _ := t.table.GetSelection()
-				row--
-				line := tview.NewInputField()
-				line.SetLabel("Filter: ")
-				t.filter = ""
-				t.filterData()
-				line.SetFieldBackgroundColor((tcell.ColorBlack))
-				line.SetChangedFunc(func(text string) {
-					t.filter = text
-					t.filterData()
-				})
-				line.SetDoneFunc(func(key tcell.Key) {
-					t.filter = line.GetText()
-					t.filterData()
-					t.updateLastLine()
-					t.app.SetFocus(t.table)
-				})
-				t.flex.RemoveItem(t.lastLine)
-				t.lastLine = line
-				t.flex.AddItem(t.lastLine, 1, 0, false)
-				t.app.SetFocus(line)
-			}
-			for _, c := range t.commands {
-				if event.Rune() == c.ch {
-					row, _ := t.table.GetSelection()
-					t.app.Suspend(func() {
-						c.action(t.orderRows[row-1])
-						t.fillTable()
-					})
-				}
-			}
-		}
-		return event
-	})
-	t.flex.SetBackgroundColor(tcell.ColorRed)
-	t.flex.SetDirection(tview.FlexRow)
-	t.flex.AddItem(t.table, 0, 1, true)
-	t.flex.AddItem(text, 1, 0, false)
-	t.lastLine = tview.NewBox()
-	t.flex.AddItem(t.lastLine, 1, 0, false)
-	t.app.SetRoot(t.flex, true)
-	if err := t.app.Run(); err != nil {
-		panic(err)
-	}
+	t.app.Run()
 }
